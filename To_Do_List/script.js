f5cc84696b1926ef755298d7ab405c4bc698496e
Ppandey12 <paraspandey827@gmail.com>
2023-01-02 19:56:49 -0500

    up tp now
    erfw

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,16 +1,47 @@
 
-const task_input = document.querySelector(".task")
-const button = document.querySelector(".add")
-const to_do = document.querySelector(".needTodo")
+// const task_input = document.querySelector(".task")
+// const button = document.querySelector(".add")
+// const to_do = document.querySelector(".needTodo")
+// const text = task_input.value
 
 
-//adds itesm
-button.addEventListener('click', addTask);
-function addTask(event) {
-    event.preventDefault();
+// //adds itesm
+// button.addEventListener("click", add_Task);
 
-    if (!task_input.value) {
-        alert("You need to add some text")
-        return;
-    }
+// function add_Task(event) {
+//     event.preventDefault();
+
+//     if (!text) {
+//         alert("You need to add some text")
+//         return;
+//     }
+
+//     //this code creates a new div called tod0
+//     const newdiv_todo = document.createElement("div")
+//     newdiv_todo.classList.add("todo")
+
+//     //this code creates a new div called do
+//     const new_div = document.createElement("div")
+//     new_div.classList.add("do")
+
+//     new_div.innerText = text
+
+//     //this code puts new_div inside newdiv_todo
+//     newdiv_todo.appendChild(new_div)
+//     to_do.appendChild(newdiv_todo)
+
+
+//     const new_input = document.createElement("input")
+//     new_input.classList.add("required")
+//     new_input.type = "text"
+//     new_input.value = text
+
+
+//     const delete_button = document.createElement("button")
+//     delete_button.classList.add("delete")
+//     delete_button.innerText = 'Delete';
+
+//     new_div.appendChild(delete_button)
+
+
 }
