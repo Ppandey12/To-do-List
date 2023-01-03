5c6f0a167d72f1293ef0e8e967e91f24de559839
Ppandey12 <paraspandey827@gmail.com>
2023-01-02 21:22:12 -0500

    finished
    As of now everythign works properly

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -2,8 +2,6 @@
 const task_input = document.querySelector(".task")
 const button = document.querySelector(".add")
 const to_do = document.querySelector(".needTodo")
-const text = task_input.value
-
 
 //adds itesm
 button.addEventListener("click", add_Task);
@@ -11,6 +9,10 @@ button.addEventListener("click", add_Task);
 function add_Task(event) {
     event.preventDefault();
 
+    // this code gets the value of what is written
+    const text = task_input.value
+
+    // this if statement makes sure that there is text or it will send an alert message
     if (!text) {
         alert("You need to add some text")
         return;
@@ -24,25 +26,33 @@ function add_Task(event) {
     const new_div = document.createElement("div")
     new_div.classList.add("do")
 
-    // new_div.innerText = text
-
     //this code puts new_div inside newdiv_todo
     newdiv_todo.appendChild(new_div)
     to_do.appendChild(newdiv_todo)
 
-
+    //this creates a new input inside new_dev
     const new_input = document.createElement("input")
     new_input.classList.add("required")
-    // new_input.type = "text"
-    new_input.value = text 
+    new_input.value = text
+    new_input.setAttribute("readonly", "readonly")
+
+    //this code resets the input field
+    task_input.value = ""
+
     new_div.appendChild(new_input)
 
-
+     // this creates a new button inside new_dev
     const delete_button = document.createElement("button")
     delete_button.classList.add("delete")
     delete_button.innerText = 'Delete';
-
     new_div.appendChild(delete_button)
+
+
+
+    // this code deletes
+    delete_button.addEventListener("click", () => {
+        newdiv_todo.removeChild(new_div)
+    })
 
 
 
