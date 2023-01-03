22957ef4d995400c5632ca58c2f1dc7e5048963f
Ppandey12 <paraspandey827@gmail.com>
2023-01-02 20:11:44 -0500

    up to now
    up to now the input works for both up and down. the css is good. and the js looks good

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,47 +1,49 @@
 
-// const task_input = document.querySelector(".task")
-// const button = document.querySelector(".add")
-// const to_do = document.querySelector(".needTodo")
-// const text = task_input.value
+const task_input = document.querySelector(".task")
+const button = document.querySelector(".add")
+const to_do = document.querySelector(".needTodo")
+const text = task_input.value
 
 
-// //adds itesm
-// button.addEventListener("click", add_Task);
+//adds itesm
+button.addEventListener("click", add_Task);
 
-// function add_Task(event) {
-//     event.preventDefault();
+function add_Task(event) {
+    event.preventDefault();
 
-//     if (!text) {
-//         alert("You need to add some text")
-//         return;
-//     }
+    if (!text) {
+        alert("You need to add some text")
+        return;
+    }
 
-//     //this code creates a new div called tod0
-//     const newdiv_todo = document.createElement("div")
-//     newdiv_todo.classList.add("todo")
+    //this code creates a new div called tod0
+    const newdiv_todo = document.createElement("div")
+    newdiv_todo.classList.add("todo")
 
-//     //this code creates a new div called do
-//     const new_div = document.createElement("div")
-//     new_div.classList.add("do")
+    //this code creates a new div called do
+    const new_div = document.createElement("div")
+    new_div.classList.add("do")
 
-//     new_div.innerText = text
+    // new_div.innerText = text
 
-//     //this code puts new_div inside newdiv_todo
-//     newdiv_todo.appendChild(new_div)
-//     to_do.appendChild(newdiv_todo)
-
-
-//     const new_input = document.createElement("input")
-//     new_input.classList.add("required")
-//     new_input.type = "text"
-//     new_input.value = text
+    //this code puts new_div inside newdiv_todo
+    newdiv_todo.appendChild(new_div)
+    to_do.appendChild(newdiv_todo)
 
 
-//     const delete_button = document.createElement("button")
-//     delete_button.classList.add("delete")
-//     delete_button.innerText = 'Delete';
+    const new_input = document.createElement("input")
+    new_input.classList.add("required")
+    // new_input.type = "text"
+    new_input.value = text 
+    new_div.appendChild(new_input)
 
-//     new_div.appendChild(delete_button)
+
+    const delete_button = document.createElement("button")
+    delete_button.classList.add("delete")
+    delete_button.innerText = 'Delete';
+
+    new_div.appendChild(delete_button)
+
 
 
 }
